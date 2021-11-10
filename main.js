0715e93f9cf0f6eb9ba431f138c55c8f99a399a9
Ishithareddy123 <81242657+Ishithareddy123@users.noreply.github.com>
2021-11-10 17:42:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,21 +4,29 @@ var recognition= new speechRecognition();
 function start(){
     document.getElementById("textarea").innerHTML="";
     recognition.start();
+
 }
 recognition.onresult=function run(event){
     console.log(event);
     content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textarea").innerHTML=content;
-    speak();
+    if(content=="Take my selfie."){
+        speak();
+    }
 }
 function speak(){
     var synth=window.speechSynthesis;
-    var speech=document.getElementById("textarea").value;
+    var speech="taking your selfie in 5 seconds";
     var speakthis=new SpeechSynthesisUtterance(speech);
     synth.speak(speakthis);
     Webcam.attach(camera);
+    setTimeout(function(){
+    take_snapshot() ;
+    save();
+    },5000);
 }
+
 Webcam.set({
     width:360,
     height:240,
@@ -26,3 +34,14 @@ Webcam.set({
     png_quality:90
 });
 camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='selfie-image' src='"+data_uri+"'>";
+    })
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie-image").src;
+    link.href=image;
+    link.click();
+}
